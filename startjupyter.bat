219060cdc05005ce6a62564e7f815a367d103cc6
llathrop <llathrop@gmail.com>
2017-09-12 15:37:30 -0500

    feat: set the train size epochs, etc

--- startjupyter.bat
+++ startjupyter.bat
@@ -1,4 +1,4 @@
-@echo off
-
-%HOMEDRIVE%
+@echo on
+c:
+cd C:\Users\llathrop\Documents\projects\udacity-AIND\class_projects\aind2-dl
 python "C:/ProgramData/Anaconda3/Scripts/jupyter-notebook-script.py" %USERPROFILE%\Documents\projects
